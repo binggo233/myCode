e0d053ae4349c3a6fd4e25f71e53c5d71b8ecd99
binggo233 <604084680@qq.com>
2016-07-12 18:40:35 +0800

    secondProject

---
+++ secondProject/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+/** 
+ * @author  zhangxf 
+ * @date 创建时间：2016年7月12日 下午6:29:21 
+ * @version 1.0 
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
